780bfd1f0a086352da93aea50a034d61f699b8ea
HSY\dbstj <immsee098@gmail.com>
2024-06-17 01:53:16 +0900

    01장

---
+++ src/main/java/kr/co/hanbit/product/management/Product.java
@@ -0,0 +1,22 @@
+package kr.co.hanbit.product.management;
+
+public class Product {
+    private Long id;
+    private String name;
+    private Integer price;
+    private Integer amount;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public Integer getPrice() {
+        return price;
+    }
+    public Integer getAmount() {
+        return amount;
+    }
+}
